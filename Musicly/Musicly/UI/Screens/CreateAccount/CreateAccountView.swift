1094bd6991a4d587e61656c1c0db3dc23aa35248
chriswebb09 <christopher.webb@flatironschool.com>
2017-05-16 03:37:53 -0400

    added guest user button setup

--- Musicly/Musicly/UI/Screens/CreateAccount/CreateAccountView.swift
+++ Musicly/Musicly/UI/Screens/CreateAccount/CreateAccountView.swift
@@ -2,10 +2,42 @@ import UIKit
 
 class CreateAccountView: UIView {
     
-    var titleLabel: UILabel!
-    var usernameField: TextFieldExtension!
-    var emailField: TextFieldExtension!
-    var passwordField: TextFieldExtension!
-    var confirmPasswordField: TextFieldExtension!
+    var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        return titleLabel
+    }()
+    
+    var usernameField: TextFieldExtension = {
+        let usernameField = TextFieldExtension()
+        return usernameField
+    }()
+    
+    var emailField: TextFieldExtension = {
+        let emailField = TextFieldExtension()
+        return emailField
+    }()
+    
+    var passwordField: TextFieldExtension = {
+        let passwordField = TextFieldExtension()
+        return passwordField
+    }()
+    
+    var confirmPasswordField: TextFieldExtension = {
+        let confirmPasswordField = TextFieldExtension()
+        return confirmPasswordField
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    func setupTitleLabel(label: UILabel) {
+        addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 0).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: UIScreen.main.bounds.height * -0.25).isActive = true
+        titleLabel.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.4).isActive = true
+        titleLabel.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.1).isActive = true
+    }
     
 }
--- Musicly/Musicly/UI/Screens/CreateAccount/CreateAccountViewController.swift
+++ Musicly/Musicly/UI/Screens/CreateAccount/CreateAccountViewController.swift
@@ -2,7 +2,12 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
     
+    let createAccountView = CreateAccountView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        createAccountView.frame = UIScreen.main.bounds
+        view.addSubview(createAccountView)
+        createAccountView.layoutSubviews()
     }
 }
